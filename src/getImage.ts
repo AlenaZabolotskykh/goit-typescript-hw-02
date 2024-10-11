3e9735754732fb239b771e7127f72c5223a87b6f
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-10-11 13:09:27 +0300

    fix getimage

--- src/getImage.ts
+++ src/getImage.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const KEY = "ed2sR4g0UcheyXSFHmZdb1MPIy4PjZh6mMedQmRfHO8";
 
-export const getImage = async (query, page) => {
+export const getImage = async (query: string, page: number): Promise<void> => {
   const { data } = await axios.get(
     `https://api.unsplash.com/search/photos?client_id=${KEY}&page=${page}&per_page=12&orientation=landscape&query=${query}`
   );
